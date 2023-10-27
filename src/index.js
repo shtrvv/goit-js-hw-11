5b8a16c7fdb8c053e1bb43dd5925fdfdfe354b56
shtrvv <shatrova.liza.21@gmail.com>
2023-10-27 04:20:53 +0300

    update hw

--- src/index.js
+++ src/index.js
@@ -1,25 +1,62 @@
 import Notiflix from 'notiflix';
-import { getPictures } from './pictures';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
 
 const refs = {
     form: document.querySelector('.search-form'),
     list: document.querySelector('.gallery'),
+    guard: document.querySelector('.js-guard'),
 }
 
 refs.form.addEventListener('submit', hadlerSearch);
+refs.list.classList.add('is-hidden');
+
+const options = {
+  root: null,
+  rootMargin: "300px",
+};
+
+const observer = new IntersectionObserver(handlerLoadMore, options);
+
+let currentPage = 1;
+let userPicture = "";
+
+export async function getPictures(userPicture) {
+    axios.defaults.baseURL = 'https://pixabay.com/api/';
+    const resp = await axios.get('', {
+        params: {
+            key: '40253142-cb6894ff2fafd8467210783e3',
+            q: `${userPicture}`,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            per_page: 40,
+            page: `${currentPage}`,
+        }
+    });
+    const pictures = await resp.data;
+    return pictures;
+}
 
 function hadlerSearch(e) {
+    refs.list.classList.remove('is-hidden');
     e.preventDefault();
     refs.list.innerHTML = '';
 
-    const userPicture = refs.form.elements.searchQuery.value;
+    userPicture = refs.form.elements.searchQuery.value;
+    currentPage = 1;
     getPictures(userPicture)
         .then((data) => {
             if (data.totalHits === 0) {
                 throw new Error(Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')); 
             }
+            if (data.hits.length < data.totalHits) {
+                observer.observe(refs.guard);
+            }
             Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
-            refs.list.insertAdjacentHTML('afterbegin', createMarkup(data));
+            refs.list.insertAdjacentHTML('beforeend', createMarkup(data));
+            galleryBox.refresh();
         })
         .catch(() => {
             console.log('ERROR');
@@ -28,10 +65,29 @@ function hadlerSearch(e) {
     refs.form.reset();
 }
 
+function handlerLoadMore(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            currentPage += 1;
+            getPictures(userPicture)
+                .then((data) => {
+                refs.list.insertAdjacentHTML('beforeend', createMarkup(data));
+                galleryBox.refresh();
+                    if (currentPage >= 13) {
+                        observer.unobserve(refs.guard);
+                }
+            })
+            .catch(() => {
+                console.log('ERROR');
+            });
+        }
+    });
+}
+
 function createMarkup(data) {
-    return data.hits.map(({webformatURL, tags, likes, views, comments, downloads}) => `
+    return data.hits.map(({largeImageURL, webformatURL, tags, likes, views, comments, downloads}) => `
     <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" width="360px" height="241px" />
+        <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width="360px" height="241px" /></a>
         <div class="info">
             <p class="info-item">
                 <b>Likes</b> ${likes}
@@ -48,3 +104,7 @@ function createMarkup(data) {
         </div>
     </div> `).join('');
 }
+
+const galleryBox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+});
